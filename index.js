8b2428897ac97f8a0c53c16dc179bf63ab1dfa4a
shahadat4536 <shahadathossain4536@gmail.com>
2022-05-17 10:09:07 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+var jwt = require("jsonwebtoken");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qnkzv.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const servicesCollection = client
+      .db("doctors_protal")
+      .collection("services");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
